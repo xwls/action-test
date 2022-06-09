9c24e7c6b0ee53cfb799b02df4140cd47193e17a
汪增伟 <351916362@qq.com>
2022-06-09 15:42:27 +0800

    测试读取Secrets

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"runtime"
 )
 
@@ -9,4 +11,18 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println(runtime.GOOS)
 	fmt.Println(runtime.GOARCH)
+	key1Bytes, err := ioutil.ReadFile("key1.txt")
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	key1 := string(key1Bytes)
+
+	key2Bytes, err := ioutil.ReadFile("key2.txt")
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	key2 := string(key2Bytes)
+	fmt.Println(key1, key2)
 }
