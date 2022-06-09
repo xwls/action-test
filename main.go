c2bc5ce858f07fc5d1c7a92682ad830ad6c6e084
汪增伟 <351916362@qq.com>
2022-06-09 16:36:23 +0800

    整理代码

--- main.go
+++ main.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"runtime"
-	"strings"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(runtime.GOOS)
 	fmt.Println(runtime.GOARCH)
-	key1Bytes, err := ioutil.ReadFile("key1.txt")
-	if err != nil {
-		log.Panicln(err)
-		return
-	}
-	key1 := string(key1Bytes)
-	key1 = strings.TrimSpace(key1)
-
-	key2Bytes, err := ioutil.ReadFile("key2.txt")
-	if err != nil {
-		log.Panicln(err)
-		return
-	}
-	key2 := string(key2Bytes)
-	key2 = strings.TrimSpace(key2)
-	fmt.Println(len(key1), key1)
-	fmt.Println(len(key2), key2)
 
 	envKey1, b := os.LookupEnv("KEY1")
 	fmt.Println(len(envKey1), envKey1, b)
